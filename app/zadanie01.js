6772b1e5571342713e36a59d6ef94e67f7e30b68
Hubert Kozłowski <hubert890@poczta.onet.pl>
2018-09-19 13:26:36 +0200

    day3

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,14 @@
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
+const passwords = ['??TegoHasła', 'CodersLab', 'Node.js Szyfruje Pliki', 'Zaźółć Gęślą Jaźń', 'Moje Haslo 1@3!', '111#$((@)n', 'Dzisiaj Szyfruje 83'];
+const algorythms = ['sha256', 'sha512', 'md5', 'rmd160']
 
-//Twój kod
+const crypto = require('crypto');
+
+passwords.forEach(pass => {
+		algorythms.forEach(alg => {
+				const result = crypto.createHmac(alg, pass);
+				if (result.digest('hex') === MY_PWD_HASH) {
+						console.log('Algorytm: ' + alg, '; Hasło: ' + pass);
+				}
+		})
+})
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,15 @@
-//Twój kod
+const fs = require('fs');
+const crypto = require('crypto');
+
+fs.readFile('./data/zadanieDnia1/testFile.txt', 'utf8', (err, data) => {
+		if (err === null) {
+				console.log('Poprawnie odczytano plik.');
+				const hash = crypto.createHmac('sha256', data)
+						.digest('hex');
+
+				console.log(hash);
+
+		} else {
+				console.log('Błąd podczas odczytu pliku!', err);
+		}
+})
